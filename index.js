4fa16462af2e1b1fe88ef3824167f3ea0cdc88f1
SipiczkiMartin <sipiczki45@gmail.com>
2021-07-14 15:01:04 +0200

    react added

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render()
+
 console.log(12);
 
 /*
 1. npm init
 2. npm install --save-dev webpack  or npm install -g yarn
 3. babel loader
+
+
+with yarn use yarn add package name
 */
